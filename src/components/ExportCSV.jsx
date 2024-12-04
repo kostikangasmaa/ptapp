f49da93f8703d97365cfe66776dae094967aef71
Kosti Kangasmaa <kangasmaa95@gmail.com>
2024-12-04 19:22:45 +0200

    Edited exportCSV.jsx

--- src/components/ExportCSV.jsx
+++ src/components/ExportCSV.jsx
@@ -3,15 +3,27 @@ import { CSVLink } from 'react-csv';
 import Button from '@mui/material/Button';
 
 function ExportCSV({ data, filename }) {
-    const headers = [
-        { label: 'First Name', key: 'firstname' },
-        { label: 'Last Name', key: 'lastname' },
-        { label: 'Street Address', key: 'streetaddress' },
-        { label: 'Postcode', key: 'postcode' },
-        { label: 'City', key: 'city' },
-        { label: 'Email', key: 'email' },
-        { label: 'Phone', key: 'phone' }
-    ];
+    let headers;
+
+    if (filename === "customers.csv") {
+        headers = [
+            { label: 'First Name', key: 'firstname' },
+            { label: 'Last Name', key: 'lastname' },
+            { label: 'Street Address', key: 'streetaddress' },
+            { label: 'Postcode', key: 'postcode' },
+            { label: 'City', key: 'city' },
+            { label: 'Email', key: 'email' },
+            { label: 'Phone', key: 'phone' }
+        ];
+    } else {
+        headers = [
+            { label: 'Date', key: 'date' },
+            { label: 'Activity', key: 'activity' },
+            { label: 'Duration', key: 'duration' },
+            { label: 'First Name', key: 'customer.firstname' },
+            { label: 'Last Name', key: 'customer.lastname' }
+        ];
+    }
 
     return (
         <CSVLink data={data} headers={headers} filename={filename}>
